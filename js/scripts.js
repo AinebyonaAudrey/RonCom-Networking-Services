7aefc0bce99d040a03152016ba020117ca29d3a1
AinebyonaAudrey <audreyainebyona@gmail.com>
2025-09-22 14:34:21 +0300

    Improved Social Media Icons' styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
-/*!
-* Start Bootstrap - Small Business v5.0.6 (https://startbootstrap.com/template/small-business)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+document.addEventListener('DOMContentLoaded', function () {
+    var yearEl = document.getElementById('year');
+    if (yearEl) yearEl.textContent = new Date().getFullYear();
+});
